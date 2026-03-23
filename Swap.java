2af13ca4e86869920028a71bffb8aa1eba646bd2
Manish  Mishra <manishweb9199@gmail.com>
2026-03-24 00:15:42 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+class Swap
+{
+public static void main(String[]args)
+{
+int a=10;
+int b=20;
+System.out.println("Befor Swapping..");
+System.out.println("a="+a);
+System.out.println("b="+b);
+
+int  c=0;
+
+c=b;
+b=a;
+a=c;
+
+System.out.println("After Swapping..");
+System.out.println("a="+a);
+System.out.println("b="+b);
+ }
+}
