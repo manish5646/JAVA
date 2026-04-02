ec0997a6c1b2cc4d6ecb9acc771b8cbadba69f1e
Manish  Mishra <manishweb9199@gmail.com>
2026-04-02 17:37:53 +0530

    Create Snake.java

---
+++ Pattern-Problem/Snake.java
@@ -0,0 +1,18 @@
+public  class Snake{
+public static void main(String[]args){
+int n=5;
+for(int i=1;i<=n;i++)
+{
+if(i % 2!= 0){
+int k=(n*i)-(n-1);
+for(int j=1;j<=n;j++)
+System.out.println(k++ + "\t");
+}
+else{
+int k=(n*i);
+for(int j=1;j<=n;j++)
+System.out.println(k-- + "\n");
+System.out.println();
+}
+}
+}
