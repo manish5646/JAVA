95970b9bc46d50c7d3e54f6ca40ccf9d5b85430c
Manish  Mishra <manishweb9199@gmail.com>
2026-04-06 23:32:45 +0530

    Create A.java

---
+++ METHOD CLASS/A.java
@@ -0,0 +1,14 @@
+class A
+{
+	public static void main(String[]args)
+{
+		int i=0 ,j=0;
+		j=m1(++i) + m1(i++) +m1(i+1);
+		System.out.println(i);
+		System.out.println(j);
+}
+static int m1(int a)
+	{
+	return a++;
+}
+}
