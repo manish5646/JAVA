50deef0bcfbf294f32652d1574712287f859e27b
Manish  Mishra <manishweb9199@gmail.com>
2026-04-11 23:38:39 +0530

    Implement overloaded main methods in F.java

---
+++ Recursion/F.java
@@ -0,0 +1,18 @@
+class F
+{
+public static void main(String[]agrs)
+{
+main();
+main(30);
+main("abcd");
+}
+public static void main(String a){
+System.out.println("main (String)");
+}
+public static void main(){
+System.out.println("main()");
+}
+public static void main(int a){
+System.out.println("main(int)");
+}
+}
