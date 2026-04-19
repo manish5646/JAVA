423d4a6c218ce1ec8036307dd7203667c74e3898
Manish  Mishra <manishweb9199@gmail.com>
2026-04-19 23:53:27 +0530

    Create Vehicle.java

---
+++ OOPS/Inheritance/Vehicle.java
@@ -0,0 +1,18 @@
+class Vehicle {
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+class Bike extends Vehicle {
+    void run() {
+        System.out.println("Bike is running safely");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Bike b = new Bike();
+        b.run();
+    }
+}
