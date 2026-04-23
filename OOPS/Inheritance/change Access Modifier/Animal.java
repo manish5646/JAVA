d0317e4e0b6b424fb6c6ac8846e424601bd85c46
Manish  Mishra <manishweb9199@gmail.com>
2026-04-23 22:51:17 +0530

    Create Animal.java

---
+++ OOPS/Inheritance/change Access Modifier/Animal.java
@@ -0,0 +1,26 @@
+Class Animal{
+  Animal m1(){
+    System.out.println("m1()-Animal);
+  }
+}
+Class Dog extends Animal{
+@override
+  protected Dog m1(){
+  System.out.println("m1()-Dog");
+}
+}
+class BabyDog exteds Dog{
+  @override
+    protected Babydog(){
+    System.out.println("m1()-BabyDog");
+  }
+}
+public class AnimalDriver{
+  public static void main(String[]args);
+  BabyDog.b=new babydog;
+  b.Babydog();
+Dog.d=new Dog;
+  d.Dog();
+}
+}
+    
