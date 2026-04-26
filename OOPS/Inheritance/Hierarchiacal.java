a0e43844e24dce727ef49648125b0fc75a5feda9
Manish  Mishra <manishweb9199@gmail.com>
2026-04-26 23:54:20 +0530

    Create Hierarchiacal.java

---
+++ OOPS/Inheritance/Hierarchiacal.java
@@ -0,0 +1,30 @@
+class Animal {
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Meowing");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        d.eat();
+        d.bark();
+
+        c.eat();
+        c.meow();
+    }
+}
