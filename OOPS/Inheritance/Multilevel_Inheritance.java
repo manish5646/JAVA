34b93e127c4fd6f63467c073314f837cbcf06b44
Manish  Mishra <manishweb9199@gmail.com>
2026-04-26 23:55:12 +0530

    Create Multilevel_Inheritance.java

---
+++ OOPS/Inheritance/Multilevel_Inheritance.java
@@ -0,0 +1,25 @@
+interface A {
+    void show();
+}
+
+interface B {
+    void display();
+}
+
+class C implements A, B {
+    public void show() {
+        System.out.println("Show from A");
+    }
+
+    public void display() {
+        System.out.println("Display from B");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.show();
+        obj.display();
+    }
+}
