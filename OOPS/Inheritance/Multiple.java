9d00e619dfa0ca38074e05e52fd3bbfcd1de3e07
Manish  Mishra <manishweb9199@gmail.com>
2026-04-26 23:56:41 +0530

    Create Multiple.java to illustrate inheritance rules
    Added a Java file demonstrating multiple inheritance restrictions.

---
+++ OOPS/Inheritance/Multiple.java
@@ -0,0 +1,11 @@
+// ❌ This will give error
+//multiple inheritance not supported
+class A {
+    void show() {}
+}
+
+class B {
+    void display() {}
+}
+
+// class C extends A, B {}  ❌ Not allowed
