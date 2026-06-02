40fe19e0c86df7a01b153f005effa53d7b10a0ed
Manish  Mishra <manishweb9199@gmail.com>
2026-06-02 21:59:10 +0530

    Create index.java

---
+++ Array/index.java
@@ -0,0 +1,9 @@
+public class index {
+     public static void main(String[]args) {
+      int[] var1 = new int[]{10, 20, 30};
+      System.out.println(var1[0]);
+      System.out.println(var1[2]);
+      System.out.println(var1.length);
+   }
+}
+
