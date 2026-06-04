fe014e793cccc14e2b42c3bab254e4680a835db8
Manish  Mishra <manishweb9199@gmail.com>
2026-06-04 22:42:14 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class BubbleSort {
+  public static void main(String[] args) {
+    int[]a={8,9,0,4,3,2,1};
+    sort(a);
+    System.out.println(Arrays.toString(a));
+  }
+  public static void sort(int[]a){
+    for(int i=0;i<a.length;i++){
+      for(int j=0;j<a.length-1-i;j++){
+        if(a[j]>a[j+1]){
+          int temp=a[j];
+          a[j]=a[j+1];
+          a[j+1]=temp;
+        }
+      }
+    }
+  }
+  }
+
