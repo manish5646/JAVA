c1d8c68ed8d796d706465d5017fded8531dcaf68
Manish  Mishra <manishweb9199@gmail.com>
2026-06-08 11:43:45 +0530

    Create MergeSort.java

---
+++ Array/MergeSort.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class MergeSort{
+  public static void main(String[] args) {
+    int[]a={9,8,4,3};
+    sort(a);
+    System.out.println(Arrays.toString(a));
+  }
+  public static void sort(int[]a){
+     if(a.length==1)
+     return;
+     int[]left =new int[a.length/2];
+     int[]rigth=new int[a.length - left.length];
+     for(int i=0;i<left.length;i++){
+      left[i]=a[i];
+      for(int j=0;j<rigth.length;j++){
+        
+      }
+       
+      sort(left);
+      sort(right);
+      merge(left,right,a);
+     }
+     public static void merge(inr[]a,int[]b,int[]c){
+      int i=0;j=0;k=0;
+      while(i<a.length && j<b.length){
+        if(a[i]<b[j])
+        c[k]=a[i];
+        k++;
+        i++;  
+      else 
+        c[k]=a[i];
+        k++;
+        i++;
+        
+      }
+      while(i<a.length)
+         c[k]=a[i];
+         k++;
+         i++;
+      }
+      while (j<b.length)
+      c[k++]=b[j++];
+     }
+     }
+  
+
+  
