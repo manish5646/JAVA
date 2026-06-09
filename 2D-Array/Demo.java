d8d14e7971994909aad4431627ce6201dd126dd9
Manish  Mishra <manishweb9199@gmail.com>
2026-06-09 12:34:14 +0530

    Create Demo.java

---
+++ 2D-Array/Demo.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {1, 2, 3, 4},
+            {5, 6},
+            {6, 7, 8, 9, 12}
+        };
+
+        System.out.println(Arrays.deepToString(a));
+    }
+}
