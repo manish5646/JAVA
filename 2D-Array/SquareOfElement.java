d259e12197e9a80d2b53c0a7f6ac778951a00e84
Manish  Mishra <manishweb9199@gmail.com>
2026-06-09 12:33:33 +0530

    Create  SquareOfElement.java

---
+++ 2D-Array/SquareOfElement.java
@@ -0,0 +1,17 @@
+public class SquareOfElement {
+    public static void main(String[] args) {
+
+        int[][] a = {
+            {1, 2},
+            {3, 4}
+        };
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = a[i][j] * a[i][j];
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
