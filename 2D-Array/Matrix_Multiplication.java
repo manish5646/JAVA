1cc111a25d3457a428be328c1240592ad706885b
Manish  Mishra <manishweb9199@gmail.com>
2026-06-10 22:38:21 +0530

    Add Matrix_Multiplication class for 3x3 matrices
    Implement matrix multiplication for two 3x3 matrices and print the result.

---
+++ 2D-Array/Matrix_Multiplication.java
@@ -0,0 +1,38 @@
+
+public class Matrix_Multiplication {
+    public static void main(String[] args) {
+
+        int[][] a = {
+                {1, 2, 3},
+                {2, 2, 3},
+                {3, 2, 1}
+        };
+
+        int[][] b = {
+                {2, 2, 1},
+                {1, 1, 4},
+                {3, 2, 5}
+        };
+
+        int[][] c = new int[a.length][a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                for (int k = 0; k < a[i].length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length; j++) {
+                System.out.print(c[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+  
+    
+  
+
